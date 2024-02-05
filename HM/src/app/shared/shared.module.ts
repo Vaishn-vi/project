d267dbce5628edd2623567b88c1509898db501b6
Vaishn-vi <vaishnaviphatangare1904@gmail.com>
2024-02-04 19:21:37 -0500

    msg

--- HM/src/app/shared/shared.module.ts
+++ HM/src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
-import {MatError, MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatIconModule} from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
---
+++ weather/src/app/weather/weather.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
+})
+export class WeatherComponent {
+
+}
